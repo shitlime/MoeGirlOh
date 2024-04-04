e59c2480dc3078f5c08c08c861bf29b3be9c95d9
shitlime <fwshitlime@outlook.com>
2024-04-05 01:17:22 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">MoeGirlOh</h1>
+
+<div align="center">为了获得所有萌娘词条。</div>
+
+----------------------------------------------
+
+## 如何使用
+
+1. 克隆仓库并进入文件夹
+```shell
+git clone https://github.com/shitlime/MoeGirlOh.git
+cd MoeGirlOh
+```
+
+2. 运行脚本
+```shell
+python moegirl-all.py
+```
+
+
+## 运行时out文件夹中的内容说明
+
++ `萌娘词条.txt`    全部词条
++ `萌娘词条a.txt`    过程中累积记录的词条（很可能不完全，用于Debug）
++ `nextPage`    用于爬取中断与恢复
+
+
+## 声明
+
+**本项目仅供学习参考，请勿用于非法用途。**  
+
+**使用本项目产生的一切后果自行承担，开发者不承担任何责任**  
---
+++ moegirl-all.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
+import os
+import time
+from html import unescape
+from urllib import request
+
+def getHTML(url: str):
+    headers = {
+    'User-Agent': '''Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'''
+    }
+    req = request.Request(url=url, headers=headers)
+    page = request.urlopen(req)
+    html = page.read().decode('utf-8')
+    return html
+
+def getTitle(html: str):
+    title = re.compile(r'<li.*title="(.*)".*</li>')
+    titles = title.findall(html)
+    return titles
+
+def getNext(html: str):
+    nextPage = re.compile(r'<a href="(\S*)" \S*>下一页.*</a>')
+    nextPageUrl = nextPage.search(html)
+    try:
+        nextPageUrl = unescape(nextPageUrl[1])    #处理html转义符
+    except TypeError:
+        return None
+    return nextPageUrl
+
+def saveTXT(data: str, path: str, name: str, mode='wb'):
+    f = open(os.path.join(path, name), mode)
+    f.write(data.encode('utf-8'))
+    f.close()
+
+def readTXT(path: str, name: str, mode='rb'):
+    f = open(os.path.join(path, name), mode)
+    s = f.read().decode('utf-8')
+    f.close()
+    return s
+
+if __name__ == '__main__':
+    # 配置
+    moegirlUrl = 'https://zh.moegirl.org.cn'
+    moegirlAllPageUrl = 'https://zh.moegirl.org.cn/Special:%E6%89%80%E6%9C%89%E9%A1%B5%E9%9D%A2'
+    url = moegirlAllPageUrl
+    nextPageUrl = ''
+    # path处填写爬取的词条存放的文件夹
+    path = "out"
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+    if os.path.isfile(os.path.join(path, 'nextPage')):
+        nextPageUrl = readTXT(path, 'nextPage')
+        if nextPageUrl != '':
+            url = nextPageUrl
+    tCount = 0
+    titlesList = []
+    while nextPageUrl != None:
+        html = getHTML(url)
+        #saveTXT(html, path, 'AllPage.html')
+        titles = getTitle(html)
+        print(titles)
+        nextPageUrl = getNext(html)
+        if nextPageUrl != None:
+            url = moegirlUrl + nextPageUrl
+        saveTXT(url, path, 'nextPage')
+        print(nextPageUrl)
+        tCount += len(titles)
+        print('当前页词条数%s\t累计词条数%s'%(len(titles), tCount))
+        titlesList.extend(titles)
+        saveTXT(unescape('\n'.join(titles)), path, '萌娘词条a.txt', 'ab')
+        
+    allTitles = unescape('\n'.join(titlesList))
+    saveTXT(allTitles, path, '萌娘词条.txt')
+    print('完成！！！')
+    #清除nextPage
+    saveTXT('', path, 'nextPage')
+    exit()
